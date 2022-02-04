9dbc01ab350dfe468889d09118a7465fdc4429e2
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-02-04 17:54:52 -0300

    fibonacci implemented

--- recursion.js
+++ recursion.js
@@ -13,3 +13,13 @@ const factorialRec = ((n) => {
     return n * factorialRec(n-1);
   }
 });
+
+const fibonacci = ((n) => {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+});
